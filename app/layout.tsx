5181208349f5dc58f21990c6155756934054e521
ferswift <nandofhen@gmail.com>
2026-05-04 14:31:45 -0300

    Subindo addDoc getDoc

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
           <nav className="flex gap-10">
             <Link href="/">Home</Link>
             <Link href="/login">Login</Link>
+            <Link href="/users">Users</Link>
           </nav>
         </header>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { db } from "@/Firebase/FireBaseConfig";
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+
 export default function Home() {
-  return <h1>Home page.</h1>;
+  const [userName, setUserName] = useState<string>("");
+
+  const handleSaveUser = async () => {
+    if (!userName.trim()) {
+      return alert("Por favor, insira um nome de usuário válido.");
+    }
+
+    try {
+      await addDoc(collection(db, "users"), {
+        name: userName,
+        createdAt: new Date(),
+      });
+
+      setUserName("");
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+
+      <h1>Enviar Usuários</h1>
+      <input
+        type="text"
+        placeholder="Digite um usuário"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+
+      <button
+        className="mt-10 p-10 border-2 border-white rounded-md"
+        disabled={!userName.trim()}
+        onClick={handleSaveUser}
+      >
+        Salvar no Firestore
+      </button>
+    </div>
+  );
 }
